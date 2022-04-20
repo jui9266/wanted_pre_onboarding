09cba02910c09e65f1463d330bf575cb9978e50f
DESKTOP-AG67U87\User <second3965@gmail.com>
2022-04-20 17:00:23 +0900

    Toggle , Tab

---
+++ src/components/Tab.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import './Tab.css'
+
+const Tab = () => {
+    const [activeIndex, setActiveIndex] = useState(0)
+
+    const onclickTab = (e) => {
+        setActiveIndex(Number(e.target.id))
+    }
+
+    return (
+        <div className='tap'>
+            <h1>Tab.js </h1>
+            <div className="tapWrap">
+                <ul>
+                    <li onClick={onclickTab} id="0" className={activeIndex === 0 ? 'active' : ''} >감자</li>
+                    <li onClick={onclickTab} id="1" className={activeIndex === 1 ? 'active' : ''} >고구마</li>
+                    <li onClick={onclickTab} id="2" className={activeIndex === 2 ? 'active' : ''} >카레라이스</li>
+                </ul>
+                <div className={`line active${activeIndex}`}></div>
+            </div>
+        </div>
+    )
+}
+
+export default Tab
---
+++ src/components/Toggle.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import './Toggle.css'
+
+const Toggle = () => {
+
+    const [toggleState, setToggleState] = useState(true)
+    const onClickToggle = (e) =>{
+        if(toggleState == e.target.id) return
+
+        setToggleState(!toggleState)
+    }
+
+    return (
+        <div className='toggle'>
+            <h1>Toggle.js </h1>
+            <div  className="toggleWrap">
+                <div className={toggleState ? 'toggleActive l0' : 'toggleActive l50'}></div>
+                <ul>
+                    <li onClick={onClickToggle} id="1" className={toggleState ? 'active' : ''}>기본</li>
+                    <li onClick={onClickToggle} id="0" className={!toggleState ? 'active' : ''}>상세</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Toggle
